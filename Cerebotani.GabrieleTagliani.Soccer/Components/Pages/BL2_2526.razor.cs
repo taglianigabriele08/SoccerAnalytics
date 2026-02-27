181231aa914278a78f5c247cd5acc81f9e5b5396
taglianigabriele08 <taglianigabriele08@gmail.com>
2026-02-27 09:23:40 +0100

    Improve code

--- Cerebotani.GabrieleTagliani.Soccer/Components/Pages/BL2_2526.razor.cs
+++ Cerebotani.GabrieleTagliani.Soccer/Components/Pages/BL2_2526.razor.cs
@@ -5,24 +5,23 @@ namespace Cerebotani.GabrieleTagliani.Soccer.Components.Pages;
 
 public partial class BL2_2526 : ComponentBase
 {
-    [Inject] 
+    [Inject]
     public IWebHostEnvironment Env { get; set; } = default!;
 
     protected IEnumerable<Dat1> Dati { get; set; } = [];
 
     protected override void OnInitialized()
     {
-        try 
+        try
         {
             var percorsoFile = Path.Combine(Env.ContentRootPath, "Data", "Bundes2_2526.json");
-            
+
             if (File.Exists(percorsoFile))
             {
                 var jsonString = File.ReadAllText(percorsoFile);
-                
-                var options = new JsonSerializerOptions 
-                { 
-                    PropertyNameCaseInsensitive = true 
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
                 };
 
                 Dati = JsonSerializer.Deserialize<IEnumerable<Dat1>>(jsonString, options) ?? [];
